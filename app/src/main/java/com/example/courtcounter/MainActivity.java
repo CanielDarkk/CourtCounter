614b15540fd3558146ba322ffc8c30f3c3ded6ef
CanielDarkk <02daniel.clark@gulllakecs.org>
2021-03-15 11:19:49 -0400

    L3.21 - Added Score A Method

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        displayForTeamA(8);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
 }
